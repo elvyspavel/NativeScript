9893cd2817c4dc2e30371063ee213a36514e450b
elvys <elvys.pavel@gmail.com>
2020-09-11 10:19:43 -0500

    working!!!

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Application } from "@nativescript/core";
+import { NoticiasService } from "../domain/noticias.service";
+
+@Component({
+    selector: "Search",
+    templateUrl: "./search.component.html",
+    
+})
+export class SearchComponent implements OnInit {
+
+    constructor(private noticias: NoticiasService) {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        this.noticias.agregar("hola!");
+        this.noticias.agregar("hola 2!");
+        this.noticias.agregar("hola 3!");
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>Application.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
